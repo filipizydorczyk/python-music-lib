ff5c352e23c671856e8fe910b9e76b1bed941d32
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2020-07-28 20:20:17 +0200

    Added sequencer class

--- musiclib/models/sequencer.py
+++ musiclib/models/sequencer.py
@@ -6,5 +6,5 @@ class Sequencer:
     def __init__(self, seqences: list):
         self.__seqences = seqences
 
-    def performe_sequence(self, start_note: Note, steps: int = len(self.__seqences)) -> list:
+    def perform_sequence(self, start_note: Note, steps: int = len(self.__seqences)) -> list:
         return None
