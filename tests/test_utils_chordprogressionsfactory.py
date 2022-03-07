ac96cbef7805299d5fbd4a0a9724689300c25999
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-03-07 21:24:16 +0100

    dev: tests for basic chords progression implementation done

--- tests/test_utils_chordprogressionsfactory.py
+++ tests/test_utils_chordprogressionsfactory.py
@@ -25,3 +25,23 @@ def test_create_chordprogression_negative():
 
     with pytest.raises(ChordProgressionStringFormat):
         result = create_chordprogression(scale,"V-1")
+
+def test_create_chordprogression_list_string_positive():
+    scale = MajorScale(Sounds.C)
+    result = create_chordprogression_list(scale,"V-I")
+
+    assert type(result) is list
+    assert len(result) == 2
+
+def test_create_chordprogression_list_enum_positive():
+    scale = MajorScale(Sounds.C)
+    result = create_chordprogression_list(scale,ChordProgressions.I_V_vi_IV)
+
+    assert type(result) is list
+    assert len(result) == 4
+
+def test_create_chordprogression_list_negative():
+    scale = MajorScale(Sounds.C)
+
+    with pytest.raises(ChordProgressionStringFormat):
+        result = create_chordprogression_list(scale,"V-1")
