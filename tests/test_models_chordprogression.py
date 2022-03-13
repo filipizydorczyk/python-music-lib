5135ba0e161d1d35ae8161ed4c695d7794b2371e
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-03-13 19:49:36 +0100

    ref: changes merge

--- tests/test_models_chordprogression.py
+++ tests/test_models_chordprogression.py
@@ -1,8 +1,7 @@
 import pytest
 
-from musiclib.models.scales import scale
-
 from .context import ChordProgression, ChordProgressions, MajorScale, Sounds, ChordProgressionStringFormat
+
 
 def test_should_throw_wrong_string_format():
     scale = MajorScale(Sounds.C)
@@ -18,10 +17,12 @@ def test_should_throw_wrong_string_format():
     with pytest.raises(ChordProgressionStringFormat):
         chord_progression = ChordProgression(scale, "I--V-vi-IV")
 
+
 def test_should_create_with_predefined_enum():
     scale = MajorScale(Sounds.C)
     chord_progression = ChordProgression(scale, ChordProgressions.I_V_vi_IV)
     assert len(chord_progression.get_chords_list()) == 4
+
 
 def test_should_create_with_correctly_formated_string():
     scale = MajorScale(Sounds.C)
