aa29799baeeea7cb2195f02bc1bf80a7c0f90fb7
Filip Izydorczyk <filip.izydorczyk@protonmail.com>
2022-04-09 23:42:01 +0200

    dev: added two m7 progressions

--- musiclib/types/chordprogressions.py
+++ musiclib/types/chordprogressions.py
@@ -7,3 +7,5 @@ class ChordProgressions(Enum):
     possible to create one from string :class:`~musiclib.models.chordprogression.ChordProgression`
     """
     I_V_vi_IV = "I-V-vi-IV"
+    Im7_Vm7_IVm7_IVm7 = "Im7-Vm7-IVm7-IVm7"
+    Im7_Vm7_IVm7_Im7 = "Im7-Vm7-IVm7-Im7"
